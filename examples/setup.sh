d9521e80a5b2a6d537475b87db62df5a992c47ee
Makis Tracend <makis.tracend@gmail.com>
2014-11-22 17:49:02 -0800

    scaffolding

---
+++ examples/setup.sh
@@ -0,0 +1,15 @@
+
+# Installing the PN:OI CLI
+
+# 1. Install the main executable
+npm install -g pnoi-cli
+
+#2. Setup PN:OI (only needed once)
+pnoi setup
+
+# you'll be propted with two entries
+# - email: ...
+# - token: ...
+
+# 3. Find out API commands using the help
+pnoi -h
